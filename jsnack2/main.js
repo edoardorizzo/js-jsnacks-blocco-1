260cfe5745889708694c20b963caecf5d0376ecd
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-14 12:19:22 +0100

    refactoring code

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -2,11 +2,11 @@ const firstWord = prompt('Inserisci la prima parola');
 const secondWord = prompt('Inserisci la seconda parola');
 
 if (firstWord.length < secondWord.length) {
-  console.log(secondWord);
   console.log(firstWord);
+  console.log(secondWord);
 } else if (firstWord.length > secondWord.length) {
-    console.log(firstWord);
     console.log(secondWord);
+    console.log(firstWord);
 } else {
     console.log('Le parole sono lunghe uguali');
 }
