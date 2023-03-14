c51804eecb100ba0c1153861169dc2b47e2bc06e
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-14 11:52:20 +0100

    add correct folder exercise

---
+++ jsnack1/main.js
@@ -0,0 +1,9 @@
+const numberOne = Number(prompt('Inserisi il primo numero'));
+const numberTwo = Number(prompt('Inserisci il secondo numero'));
+
+if (numberOne > numberTwo) {
+    console.log(numberOne);
+
+} else {
+    console.log(numberTwo);
+}
