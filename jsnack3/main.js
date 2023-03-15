f7c2a4827f7ee6720163753a2b0d5a80f96f80ed
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-15 11:48:08 +0100

    add while cycle

--- jsnack3/main.js
+++ jsnack3/main.js
@@ -1,8 +1,20 @@
 let somma = 0;
 
-for (let i = 1; i <= 10; i++) { 
+for (let i = 1; i <= 3; i++) { 
   let number = parseInt(prompt("Inserisci un numero")); 
   somma = somma + number;
+}
+
+console.log(`La somma di tutti i numeri è: ${somma}`);
+
+// while
+
+let jo = 0;
+
+while (jo <= 3) {
+  let number = parseInt(prompt("Inserisci un numero")); 
+  somma = somma + number;
+  jo++
 }
 
 console.log(`La somma di tutti i numeri è: ${somma}`);
